d3474f464f72fe88ea23db28efddca94fc5edc5c
hig0ni <rjsgml658@naver.com>
2023-10-20 01:50:16 +0900

    hero.js수정

--- components/home/hero.js
+++ components/home/hero.js
@@ -9,10 +9,10 @@ export default function Hero() {
             </div>
             <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                안녕하세요!
-                <br className="hidden lg:inline-block"/>
+                안녕하세요! 
+                <br className="inline-block"/>
                 풀스택 개발자를 꿈꾸는
-                <br className="hidden lg:inline-block"/>
+                <br className="inline-block"/>
                 김건희입니다.
                 </h1>
                 {/* <p className="mb-8 leading-relaxed">내용추가</p> */}
